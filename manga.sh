a75ff5921ad532d7717a29924a41bb734636cea9
Renato S. Martins <smartins.renato@gmail.com>
2013-08-25 10:18:32 +0200

    initial commit; submodule manga_downloader

---
+++ manga.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# manga -d ~/Ebooks/manga/berserk berserk
+# cbz2pdf Ebooks/manga/berserk/berserk.\[mr\].berserk.013.cbz
+
+#TODO: use manga_downloader to download zip file into /tmp directory
+#TODO: setup an XML configuration file
+#TODO: extract zip to /tmp/somethingsomething and `convert` images into pdf (see cbz2pdf)
